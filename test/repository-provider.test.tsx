299964c69436f0f9beb0a7b2cb60a8657796cd6e
blaskodaniel <blaskoi@gmail.com>
2019-11-14 15:32:31 +0100

    update repository-provider test

--- test/repository-provider.test.tsx
+++ test/repository-provider.test.tsx
@@ -1,14 +1,15 @@
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import React from 'react'
+import { act } from 'react-dom/test-utils'
 import { RepositoryProvider } from '../src/context/repository-provider'
 
-describe('Repository Context Provider', () => {
-  it('Matches snapshot', () => {
-    const l = shallow(
-      <RepositoryProvider>
-        <>test</>
-      </RepositoryProvider>,
-    )
-    expect(l).toMatchSnapshot()
+describe('Repository Provider', () => {
+  it('Repository Provider snapshot', async () => {
+    let wrapper: any
+    await act(async () => {
+      wrapper = mount(<RepositoryProvider />)
+    })
+
+    expect(wrapper).toMatchSnapshot()
   })
 })
