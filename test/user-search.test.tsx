8c08af03dafb1f3d688706db08d7f4fd54b9258d
blaskodaniel <blaskoi@gmail.com>
2019-11-15 13:30:07 +0100

    add some test case to user-search.test

--- test/user-search.test.tsx
+++ test/user-search.test.tsx
@@ -7,8 +7,11 @@ import Button from '@material-ui/core/Button'
 import TableRow from '@material-ui/core/TableRow'
 import TableBody from '@material-ui/core/TableBody'
 import { Dialog } from '@material-ui/core'
+import MaterialTextField from '@material-ui/core/TextField'
+import { AdvancedSearch } from '@sensenet/search-react/dist/Components/AdvancedSearch'
 import UserSearchPanel from '../src/components/user-search'
 import { TestUserList } from './_mocks_/test_contents'
+//import { PresetField } from '@sensenet/search-react/dist/Components/Fields/PresetField'
 
 describe('The user search component instance', () => {
   let wrapper: any
@@ -162,5 +165,92 @@ describe('The user search component instance', () => {
         .find(Dialog)
         .prop('open'),
     ).toBe(false)
+  })
+
+  it('should change query if change any field', async () => {
+    await act(async () => {
+      wrapper = mount(
+        <RepositoryContext.Provider value={repo as any}>
+          <UserSearchPanel />
+        </RepositoryContext.Provider>,
+      )
+    })
+    const advsearchpanel = wrapper.update().find(AdvancedSearch)
+    act(() => {
+      advsearchpanel.prop('onQueryChanged')("LoginName:'*businesscat*'")
+    })
+
+    const querytextfield = wrapper
+      .update()
+      .find(MaterialTextField)
+      .find("[label='Full query']")
+
+    expect(querytextfield.at(1).props().value).toEqual("LoginName:'*businesscat*'")
+  })
+
+  it('should change language dropdown', async () => {
+    await act(async () => {
+      wrapper = mount(
+        <RepositoryContext.Provider value={repo as any}>
+          <UserSearchPanel />
+        </RepositoryContext.Provider>,
+      )
+    })
+    const dropdownlang = wrapper.update().find(AdvancedSearch)
+
+    act(() => {
+      dropdownlang.prop('onQueryChanged')('Language:English')
+    })
+
+    const querytextfield = wrapper
+      .update()
+      .find(MaterialTextField)
+      .find("[label='Full query']")
+
+    expect(querytextfield.at(1).props().value).toEqual('Language:English')
+  })
+
+  it('should change Gender dropdown', async () => {
+    await act(async () => {
+      wrapper = mount(
+        <RepositoryContext.Provider value={repo as any}>
+          <UserSearchPanel />
+        </RepositoryContext.Provider>,
+      )
+    })
+    const dropdownlang = wrapper.update().find(AdvancedSearch)
+
+    act(() => {
+      dropdownlang.prop('onQueryChanged')('Gender:Female')
+    })
+
+    const querytextfield = wrapper
+      .update()
+      .find(MaterialTextField)
+      .find("[label='Full query']")
+
+    expect(querytextfield.at(1).props().value).toEqual('Gender:Female')
+  })
+
+  it('should change MaritalStatus dropdown', async () => {
+    await act(async () => {
+      wrapper = mount(
+        <RepositoryContext.Provider value={repo as any}>
+          <UserSearchPanel />
+        </RepositoryContext.Provider>,
+      )
+    })
+    const dropdownlang = wrapper.update().find(AdvancedSearch)
+
+    act(() => {
+      dropdownlang.prop('onQueryChanged')('MaritalStatus:Single')
+    })
+
+    const querytextfield = wrapper
+      .update()
+      .find(MaterialTextField)
+      .find("[label='Full query']")
+
+    expect(querytextfield.at(1).props().value).toEqual('MaritalStatus:Single')
   })
 })
